1cdc26dfef081eca326fdc619fa65505081f96ff
Yuta <jackhill_0128@yahoo.co.jp>
2023-04-12 20:22:20 +0900

    取得/クリックイベント/保存の定義

--- form/app.js
+++ form/app.js
@@ -1,8 +1,28 @@
-const button = document.getElementsByTagName("button");
+window.onload = function(){
+    //データ取得
+    const savedEmail = localStorage.getItem("email");
+    const savedPass = localStorage.getItem("password");
+    //DOM(取得先定義)
+    const email = document.getElementsByName("email")
+    const password = document.getElementsByName("password")
 
-//クリックイベント
-button.addEventListener("click", function(){
-//データ保存    
-    localStorage.setItem('email', email)
-}
-)
+    if(savedEmail == null){
+        email.value = savedEmail;
+        console.log(savedEmail)
+    }
+    if(savedPass == null){
+        email.value = savedPass;
+        console.log(savedPass)
+    }
+};
+
+    //クリックイベント
+    const btn = document.getElementsByTagName("button");
+    btn.addEventListener("click",function(){
+
+            const inputEmail = document.getElementsByName("email").value;
+            const inputPass = document.getElementsByName("password").value;
+
+            localStorage.setItem("email", inputEmail);
+            localStorage.setItem("password", inputPass);
+        },false);
